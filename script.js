4cdee0e5ab360614b82ac0c6e168cd4a38a095f3
nageshwari159 <mudavathnagi9@gmail.com>
2025-11-13 18:46:01 +0530

    implemented dijkstra's algorithm for shortest path calculation

--- script.js
+++ script.js
@@ -81,3 +81,58 @@
                 this.roads = [];
             }
         }
+
+class TrafficGraph{
+ constructor(numVertices){
+  this.numVertices = numVertices;
+  this.nodes = [];
+  this.adjacencyList = {}
+  this.nameTold = {};
+ }
+
+addVertex(id,name,type){
+ this.nodes.push(new Node(id,name,type));
+ this.nameTold[name] = id;
+ this.adjacencyList[id] = [];
+}
+
+addEdge(from,to,weight,roadName){
+ this.adacencyList[from].push(new Edge(to,weight,roadName));
+this.adjacencyList[to].push(new Edge(from,weight,roadName));
+}
+
+dijkstra(source,destination){
+ const distance = new
+Array(this.numVertices.).fill(INF);
+ const parent = new
+Array(this.numVertices).fill(-1);
+ const roadUsed = new
+Array(this.numVertices.fill("");
+ const minHeap = new MinHeap();
+
+ distance[source] = 0;
+ minHeap.push(source,0);
+
+ while(!minHeap.isEmpty()){
+  const current = minHeap.pop();
+  const u = current.vertex;
+
+  if(current.distance > distance[u])
+   continue;
+
+  for (const edge of this.adjacencyList[u]){
+   const v = edge.to;
+   const weight = edge.weight;
+
+   if(distance[u] +weight<distance[v]){
+    distance[v]=distance[u]+weight;
+    parent[v] = u;
+    roadUsed[v]=edge.roadName;
+    minHeap.push(v,distance[v]);
+   }
+  }
+ }
+  
+
+ 
+ 
