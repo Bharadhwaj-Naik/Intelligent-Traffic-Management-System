6c1be5f85dd0154a369b9d7a1408c67179a1683d
Bharadhwaj-Naik <bharadhwaj.naik@gmail.com>
2025-11-13 18:19:16 +0530

    implement MinHeap class with bubble operations

--- script.js
+++ script.js
@@ -15,3 +15,63 @@
                 this.roadName = roadName;
             }
         }
+        class HeapNode {
+            constructor(vertex, distance) {
+                this.vertex = vertex;
+                this.distance = distance;
+            }
+        }
+
+        class MinHeap {
+            constructor() {
+                this.heap = [];
+            }
+
+            push(vertex, distance) {
+                this.heap.push(new HeapNode(vertex, distance));
+                this.bubbleUp(this.heap.length - 1);
+            }
+
+            pop() {
+                if (this.heap.length === 0) return null;
+                if (this.heap.length === 1) return this.heap.pop();
+
+                const min = this.heap[0];
+                this.heap[0] = this.heap.pop();
+                this.bubbleDown(0);
+                return min;
+            }
+
+            bubbleUp(index) {
+                while (index > 0) {
+                    const parentIndex = Math.floor((index - 1) / 2);
+                    if (this.heap[index].distance >= this.heap[parentIndex].distance) break;
+                    [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+                    index = parentIndex;
+                }
+            }
+
+            bubbleDown(index) {
+                while (true) {
+                    let minIndex = index;
+                    const leftChild = 2 * index + 1;
+                    const rightChild = 2 * index + 2;
+
+                    if (leftChild < this.heap.length && this.heap[leftChild].distance < this.heap[minIndex].distance) {
+                        minIndex = leftChild;
+                    }
+                    if (rightChild < this.heap.length && this.heap[rightChild].distance < this.heap[minIndex].distance) {
+                        minIndex = rightChild;
+                    }
+                    if (minIndex === index) break;
+
+                    [this.heap[index], this.heap[minIndex]] = [this.heap[minIndex], this.heap[index]];
+                    index = minIndex;
+                }
+            }
+
+            isEmpty() {
+                return this.heap.length === 0;
+            }
+        }
+        
