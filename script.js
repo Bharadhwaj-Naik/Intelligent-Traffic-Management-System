5c5373e7bc4ffec29814dadc70de4a85f68ee682
sugam7 <141832531+Sugamumale@users.noreply.github.com>
2025-11-13 21:32:09 +0530

    corrected a small error in displayNodes function

--- script.js
+++ script.js
@@ -131,8 +131,7 @@ class TrafficGraph {
         nodesGrid.innerHTML = '';
 
         for (const node of this.nodes) {
-            const nodeCard = document.createElement('div');
-            nodeCard.className = `node-card ${node.type === 'emergency_station' ? 'emergency' : ''} ${node.type === 'landmark' ? 'landmark' : ''}`;
+            const nodeCard = document.createElement('div');nodeCard.className = `node-card ${node.type === 'emergency_station' ? 'emergency' : ''} ${node.type === 'landmark' ? 'landmark' : ''}`;
             
             nodeCard.innerHTML = `
                 <div class="node-id">${node.id}</div>
